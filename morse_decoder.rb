b5b85b633658edfb55789274ce6b9c07f3717e32
Favourezeugwa <favourezeugwa@gmail.com>
2022-08-24 21:49:50 +0100

    Update morse_decoder.rb file

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,32 +1,32 @@
 # decode single character
 def decode_char(char)
   morse_char = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G", 
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--" => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "X"
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'X'
   }
 
   morse_char[char]
@@ -34,10 +34,10 @@ end
 
 #  split word pattern and decode char
 def decode_word(word)
-  split_word = word.split(" ")
-  result = ""
+  split_word = word.split(' ')
+  result = ''
 
-  split_word.each { |char| result += decode_char(char)}
+  split_word.each { |char| result += decode_char(char) }
 
   result
 end
